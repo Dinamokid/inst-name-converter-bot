369838a9f26907fd813addc997c877283d5fcb24
Avdonichev Aleksandr <dinamokid@yandex.ru>
2020-09-21 22:14:13 +0300

    dev

--- logic.py
+++ logic.py
@@ -1,15 +1,14 @@
 import requests
 
-def nick_to_id(nickname, offset=None, timeout=30):  
+def nick_to_id(nickname, offset=None, timeout=30):
     params = {'timeout': timeout, 'offset': offset}
     resp = requests.get("https://www.instagram.com/{}/?__a=1".format(nickname) , params)
-    return resp.json()['graphql']['user']['id']
+    return resp.json()
 
-def id_to_nick(user_id, offset=None, timeout=30): 
+def id_to_nick(user_id, offset=None, timeout=30):
     params = {'timeout': timeout, 'offset': offset}
     resp = requests.get("https://i.instagram.com/api/v1/users/{}/info/".format(user_id) , params)
-    try: 
-        return resp.json()['user']['username']
-    except: 
+    try:
+        return resp.json()
+    except:
         return "Json crush"
-     
